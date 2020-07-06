9eb0c5d1e52ca6ec6831007e6aca525401f6b409
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2020-07-06 19:43:13 -0300

    :sparkles: Started project click
    close #7

---
+++ pycutter.py
@@ -0,0 +1,15 @@
+import click
+
+APP_NAME = "pycutter"
+
+
+@click.command()
+def main():
+    """
+    Cookiecutter CLI to start projects Python
+    """
+    print("Start project")
+
+
+if __name__ == "__main__":
+    main()
