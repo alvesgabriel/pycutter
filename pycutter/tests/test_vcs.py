9e7a7dd754d38beac5c41bbcef0d7caa52ffdf94
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2020-08-20 12:29:38 -0300

    :white_check_mark: Created test to add git
    close #35

--- pycutter/tests/test_vcs.py
+++ pycutter/tests/test_vcs.py
@@ -6,12 +6,27 @@ from pycutter.command import vcs
 
 
 @pytest.fixture
-def make_dir():
+def directory():
     dirname = "/tmp/xpto"
     yield dirname
     shutil.rmtree(dirname)
 
 
-def test_create_dir(make_dir):
-    vcs.create_dir(make_dir)
-    assert os.path.isdir(make_dir)
+def test_create_dir(directory):
+    vcs.create_dir(directory)
+    assert os.path.isdir(directory)
+
+
+def test_gitignore(directory):
+    vcs.create_dir(directory)
+    vcs.gitignore(directory)
+    gitignore = os.path.join(directory, ".gitignore")
+    assert os.path.isfile(gitignore)
+    assert os.path.getsize(gitignore)
+
+
+def test_git_init(directory):
+    vcs.create_dir(directory)
+    vcs.git_init(directory)
+    git_dir = os.path.join(directory, ".git")
+    assert os.path.isdir(git_dir)
